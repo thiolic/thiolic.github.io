9953e48f060071f0cea0af6d56762fb5a02339fd
Sanya Marynych <akinor.ua@gmail.com>
2018-12-10 23:36:31 +0300

    menu - corrected code

--- menu/app/js/common.js
+++ menu/app/js/common.js
@@ -4,11 +4,11 @@ $(function () {
     $('.menu-toggle').on('click', function () {
         if($(this).hasClass('-is-open')) {
             $(this).removeClass('-is-open');
-            $('.menu, .nav-wrapper, .menu-wrapper').removeClass('-is-open');
+            $('.menu, .menu-wrapper, .header-wrapper').removeClass('-is-open');
             $(document.body).removeClass('-overflow-hidde');
         } else {
             $(this).addClass('-is-open');
-            $('.menu, .nav-wrapper, .menu-wrapper').addClass('-is-open');
+            $('.menu, .menu-wrapper, .header-wrapper').addClass('-is-open');
             $(document.body).addClass('-overflow-hidde');
         }
     });
