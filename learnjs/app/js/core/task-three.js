3680e7292b4dfb3a406a8af9fbc2cd0234ffe3d7
Sanya <akinor.ua@gmail.com>
2019-03-04 23:49:09 +0200

    learnjs

--- learnjs/app/js/core/task-three.js
+++ learnjs/app/js/core/task-three.js
@@ -1,29 +1,22 @@
 window.addEventListener('load', () => {
-    // let arr1 = [1, 3, -5, 2, 7, 11, -6, 6, 4, -6];
-    // let arr2;
-    // let index = 0;
-    // let summ = 0;
-    //
-    // console.log('Начальный массив: ' + arr1);
-    //
-    // const firstNegativeValue = () => {
-    //     for (let i = 0; i < arr1.length; i++) {
-    //         if (arr1[i] < 0) {
-    //             return index = i;
-    //         }
-    //     }
-    // };
-    //
-    // firstNegativeValue();
-    //
-    // console.log('Индекс первого отрицательного числа: ' + index);
-    //
-    // arr2 = arr1.splice(index + 1, arr1.length);
-    //
-    // for (let i = 0; i < arr2.length; i++) {
-    //     summ += arr2[i];
-    // }
-    //
-    // console.log('Массив после: ' + arr2);
-    // console.log('Сумма: ' + summ);
+    let arr1 = [1, 3, -5, 2, 7, 11, -6, 6, 4, -6];
+    let arr2;
+    let summ = 0;
+    let negative = 0;
+
+    console.log('Начальный массив: ' + arr1);
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] < 0 && negative === 0) {
+            arr2 = arr1.splice(i + 1, arr1.length);
+            negative = 1;
+        }
+    }
+
+    for (let i = 0; i < arr2.length; i++) {
+        summ += Math.abs(arr2[i]);
+    }
+
+    console.log('Массив после: ' + arr2);
+    console.log('Сумма: ' + summ);
 });
