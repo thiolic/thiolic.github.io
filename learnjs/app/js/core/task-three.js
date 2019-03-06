f59c3f03383d1f0107f0d2a4f27d671bc567e2c6
Sanya <akinor.ua@gmail.com>
2019-03-06 23:08:39 +0200

    learnjs

--- learnjs/app/js/core/task-three.js
+++ learnjs/app/js/core/task-three.js
@@ -1,15 +1,21 @@
 window.addEventListener('load', () => {
-    let arr1 = [1, 3, -5, 2, 7, 11, -6, 6, 4, -6];
+    let arr1 = [-9, 3, -5, 2, 7, 11, -6, 6, 4, -8];
     let arr2 = [];
     let arr3 = [121, 25, 57];
     let arr4;
     let arr5;
     let arr6 = arr1.slice();
+    let arr7 = arr1.slice();
     let summ = 0;
     let summ2 = 0;
     let summ3 = 0;
     let negative = 0;
     let avg = 0;
+    let minValue;
+    let maxValue;
+    let minValueIndex;
+    let maxValueIndex;
+    let summ4 = 0;
 
     console.log('Начальный массив: ' + arr1);
 
@@ -57,4 +63,33 @@ window.addEventListener('load', () => {
     avg = summ3 / arr6.length;
 
     console.log('Среднее арифметическое положительных элементов массива: ' + avg);
+
+    minValue = Math.min.apply(null, arr7);
+    maxValue = Math.max.apply(null, arr7);
+
+    console.log('Минимальное значение в массиве: ' + minValue);
+    console.log('Максимальное значение в массиве: ' + maxValue);
+
+    minValueIndex = arr7.indexOf(minValue);
+    maxValueIndex = arr7.indexOf(maxValue);
+
+    // console.log(minValueIndex);
+    // console.log(maxValueIndex);
+
+    arr7.forEach((elem, index) => {
+        if (minValueIndex > maxValueIndex) {
+            if (index < minValueIndex && index > maxValueIndex) {
+                console.log(elem);
+                summ4 += elem;
+            }
+        } else {
+            if (index > minValueIndex && index < maxValueIndex) {
+                console.log(elem);
+                summ4 += elem;
+            }
+        }
+    });
+
+    console.log('Сумма элементов между минимальным и максимальным элементами: ' + summ4);
+
 });
