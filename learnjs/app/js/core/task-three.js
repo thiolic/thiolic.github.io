e2e9114f413112433f56d1abe7150ca50ec05de5
Sanya Marynych <olexandr.marinych.stratis@gmail.com>
2019-03-06 10:10:04 +0200

    learnjs

--- learnjs/app/js/core/task-three.js
+++ learnjs/app/js/core/task-three.js
@@ -4,9 +4,12 @@ window.addEventListener('load', () => {
     let arr3 = [121, 25, 57];
     let arr4;
     let arr5;
+    let arr6 = arr1.slice();
     let summ = 0;
     let summ2 = 0;
+    let summ3 = 0;
     let negative = 0;
+    let avg = 0;
 
     console.log('Начальный массив: ' + arr1);
 
@@ -23,6 +26,7 @@ window.addEventListener('load', () => {
 
     console.log('Массив после: ' + arr2);
     console.log('Сумма: ' + summ);
+    console.log('Номер минимального по модули элемента: ' + Math.min.apply(null, arr1.map(Math.abs)));
     console.log('Третий массив: ' + arr3);
 
     arr4 = arr3.toString().split('');
@@ -43,4 +47,14 @@ window.addEventListener('load', () => {
 
     console.log('Массив чисел: ' + arr5);
     console.log('Сумма цифр массива: ' + summ2);
+
+    arr6.forEach((elem) => {
+        if (elem > 0) {
+            summ3 += elem;
+        }
+    });
+
+    avg = summ3 / arr6.length;
+
+    console.log('Среднее арифметическое положительных элементов массива: ' + avg);
 });
