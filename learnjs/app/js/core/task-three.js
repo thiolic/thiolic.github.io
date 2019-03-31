0a8f2bd07654b9ac6a0b13cdd199bc6d2d071986
Sanya <akinor.ua@gmail.com>
2019-03-31 22:37:41 +0300

    learnjs

--- learnjs/app/js/core/task-three.js
+++ learnjs/app/js/core/task-three.js
@@ -18,77 +18,77 @@ window.addEventListener('load', () => {
 
     // Task 1
 
-    // console.log('Начальный массив: ' + arr1);
-    //
-    // arr2 = arr1.filter((elem) => {
-    //     if (negative) {
-    //         return summ += Math.abs(elem);
-    //     }
-    //
-    //     if (elem < 0 && !negative) {
-    //         negative = 1;
-    //     }
-    // });
-    //
-    // console.log('Массив после: ' + arr2);
-    // console.log('Сумма: ' + summ);
-    //
-    // //Task 2
-    //
-    // console.log('Номер минимального по модули элемента: ' + Math.min.apply(null, arr1.map(Math.abs)));
-    //
-    // //Task 3
-    //
-    // console.log('Третий массив: ' + arr3);
-    //
-    // arr3.toString().split('').map((elem) => {
-    //     if (elem != ',') {
-    //         return summ2 += +elem;
-    //     }
-    // });
-    //
-    // console.log('Сумма цифр массива: ' + summ2);
+    console.log('Начальный массив: ' + arr1);
 
-    //Task 4
+    arr2 = arr1.filter((elem) => {
+        if (negative) {
+            return summ += Math.abs(elem);
+        }
+
+        if (elem < 0 && !negative) {
+            negative = 1;
+        }
+    });
+
+    console.log('Массив после: ' + arr2);
+    console.log('Сумма: ' + summ);
+
+    //Task 2
+
+    console.log('Номер минимального по модули элемента: ' + Math.min.apply(null, arr1.map(Math.abs)));
+
+    //Task 3
+
+    console.log('Третий массив: ' + arr3);
+
+    arr3.toString().split('').map((elem) => {
+        if (elem != ',') {
+            return summ2 += +elem;
+        }
+    });
+
+    console.log('Сумма цифр массива: ' + summ2);
+
+    // Task 4
 
 
-    // minValue = Math.min.apply(null, arr7);
-    // maxValue = Math.max.apply(null, arr7);
-    //
-    // console.log('Минимальное значение в массиве: ' + minValue);
-    // console.log('Максимальное значение в массиве: ' + maxValue);
-    //
-    // minValueIndex = arr7.indexOf(minValue);
-    // maxValueIndex = arr7.indexOf(maxValue);
-    //
-    // arr7.forEach((elem, index) => {
-    //     if (minValueIndex > maxValueIndex) {
-    //         if (index < minValueIndex && index > maxValueIndex) {
-    //             console.log(elem);
-    //             summ4 += elem;
-    //         }
-    //     } else {
-    //         if (index > minValueIndex && index < maxValueIndex) {
-    //             console.log(elem);
-    //             summ4 += elem;
-    //         }
-    //     }
-    // });
-    //
-    // console.log('Сумма элементов между минимальным и максимальным элементами: ' + summ4);
+    minValue = Math.min.apply(null, arr7);
+    maxValue = Math.max.apply(null, arr7);
+
+    console.log('Минимальное значение в массиве: ' + minValue);
+    console.log('Максимальное значение в массиве: ' + maxValue);
+
+    minValueIndex = arr7.indexOf(minValue);
+    maxValueIndex = arr7.indexOf(maxValue);
+
+    arr7.forEach((elem, index) => {
+        if (minValueIndex > maxValueIndex) {
+            if (index < minValueIndex && index > maxValueIndex) {
+                console.log(elem);
+                summ4 += elem;
+            }
+        } else {
+            if (index > minValueIndex && index < maxValueIndex) {
+                console.log(elem);
+                summ4 += elem;
+            }
+        }
+    });
+
+    console.log('Сумма элементов между минимальным и максимальным элементами: ' + summ4);
 
     // Task 5
 
-    // console.log('Массив для вычисления среднего арифметического: ' + arr6);
-    //
-    // arr5 = arr6.filter((elem) => {
-    //     if (elem > 0) {
-    //         return summ3 += elem;
-    //     }
-    // });
-    //
-    // avg = summ3 / arr5.length;
-    //
-    // console.log('Среднее арифметическое положительных элементов массива: ' + avg);
+    console.log('Массив для вычисления среднего арифметического: ' + arr6);
+
+    arr5 = arr6.filter((elem) => {
+        if (elem > 0) {
+            return summ3 += elem;
+        }
+    });
+
+    avg = summ3 / arr5.length;
+
+    console.log('Среднее арифметическое положительных элементов массива: ' + avg);
 
 });
