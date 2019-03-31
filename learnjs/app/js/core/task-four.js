28c9cb60230730b2460a8c14851058b3dbf15954
Sanya <akinor.ua@gmail.com>
2019-03-31 22:04:20 +0300

    learnjs - task five

--- learnjs/app/js/core/task-four.js
+++ learnjs/app/js/core/task-four.js
@@ -1,66 +1,66 @@
-window.addEventListener('load', () => {
-    let arr1 = [9, 3, -5, 2, -11, -6, 6, 2,  4, 4, -8];
-    let arr2 = arr1.slice();
-    let arr3 = [];
-    let minNegativeValue = 0;
-    let minNegativeIndex = 0;
-    let counter = [];
-    let currentElem;
-    let arr4 = arr1.slice();
-    let arr5 = arr1.slice();
-    let arr6 = [];
-    let arr7 = arr1.slice();
-    let step = 3;
-
-    // Task 1
-
-    // arr1.forEach((elem, index) => {
-    //     if (minNegativeValue > elem) {
-    //         minNegativeValue = elem;
-    //         minNegativeIndex = index;
-    //     }
-    // });
-    //
-    // console.log(minNegativeValue, minNegativeIndex);
-
-    // Task 2
-
-    // console.log('Начальный массив: ' + arr2);
-    //
-    // arr3 = arr2.reduce((current, elem) => {
-    //     let count = 0;
-    //
-    //     if (current === elem) {
-    //         count++;
-    //     }
-    //
-    //     counter.push(count);
-    //     console.log(current, elem);
-    //
-    //     return elem;
-    // });
-    //
-    // console.log(arr3);
-    // console.log(counter);
-
-    // Task 4
-
-    console.log(arr7);
-
-    arr7.splice(2, 2 +step, 0);
-
-    console.log(arr7);
-
-    // Task 5
-
-    // arr6 = arr5.filter((elem, index, arr) => {
-    //     console.log(arr.indexOf(elem), elem, index);
-    //     return arr.indexOf(elem) === index;
-    // });
-    //
-    // console.log('Неповторяющиеся элементы массива: ' + arr6);
-
-    // Task 6
-
-    // console.log(arr4.reverse());
-});
+// window.addEventListener('load', () => {
+//     let arr1 = [9, 3, -5, 2, -11, -6, 6, 2,  4, 4, -8];
+//     let arr2 = arr1.slice();
+//     let arr3 = [];
+//     let minNegativeValue = 0;
+//     let minNegativeIndex = 0;
+//     let counter = [];
+//     let currentElem;
+//     let arr4 = arr1.slice();
+//     let arr5 = arr1.slice();
+//     let arr6 = [];
+//     let arr7 = arr1.slice();
+//     let step = 3;
+//
+//     // Task 1
+//
+//     // arr1.forEach((elem, index) => {
+//     //     if (minNegativeValue > elem) {
+//     //         minNegativeValue = elem;
+//     //         minNegativeIndex = index;
+//     //     }
+//     // });
+//     //
+//     // console.log(minNegativeValue, minNegativeIndex);
+//
+//     // Task 2
+//
+//     // console.log('Начальный массив: ' + arr2);
+//     //
+//     // arr3 = arr2.reduce((current, elem) => {
+//     //     let count = 0;
+//     //
+//     //     if (current === elem) {
+//     //         count++;
+//     //     }
+//     //
+//     //     counter.push(count);
+//     //     console.log(current, elem);
+//     //
+//     //     return elem;
+//     // });
+//     //
+//     // console.log(arr3);
+//     // console.log(counter);
+//
+//     // Task 4
+//
+//     console.log(arr7);
+//
+//     arr7.splice(2, 2 +step, 0);
+//
+//     console.log(arr7);
+//
+//     // Task 5
+//
+//     // arr6 = arr5.filter((elem, index, arr) => {
+//     //     console.log(arr.indexOf(elem), elem, index);
+//     //     return arr.indexOf(elem) === index;
+//     // });
+//     //
+//     // console.log('Неповторяющиеся элементы массива: ' + arr6);
+//
+//     // Task 6
+//
+//     // console.log(arr4.reverse());
+// });
