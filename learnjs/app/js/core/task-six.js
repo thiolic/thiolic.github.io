e8b55a54f850666c8295dbcf3d39f0bac851e14b
Sanya <akinor.ua@gmail.com>
2019-04-11 22:24:50 +0300

    learnjs

--- learnjs/app/js/core/task-six.js
+++ learnjs/app/js/core/task-six.js
@@ -1,31 +1,43 @@
 window.addEventListener('load', () => {
-    let sum = (array) => {
-        let sumOfElements = array.reduce((sum, elem) => {
-            return sum + elem;
-        }, 0);
+    // let sum = (array) => {
+    //     let sumOfElements = array.reduce((sum, elem) => {
+    //         return sum + elem;
+    //     }, 0);
+    //
+    //     return sumOfElements;
+    // };
+    //
+    // let range = (startValue, endValue, step) => {
+    //     step = step || 1;
+    //     let array = [];
+    //
+    //     if (startValue < endValue) {
+    //         for (let i = startValue; i <= endValue; i = i + step) {
+    //             array.push(i);
+    //         }
+    //     } else {
+    //         for (let i = startValue; i >= endValue; i = i + step) {
+    //             array.push(i);
+    //         }
+    //     }
+    //
+    //     console.log(array);
+    //     return array;
+    // };
+    //
+    // console.log(range(1, 10));
+    // console.log(sum(range(1, 10, 3)));
+    // console.log(sum(range(10, 1, -2)));
 
-        return sumOfElements;
-    };
+    let arrayToList = (arr) => {
+        let list = {};
 
-    let range = (startValue, endValue, step) => {
-        step = step || 1;
-        let array = [];
-
-        if (startValue < endValue) {
-            for (let i = startValue; i <= endValue; i = i + step) {
-                array.push(i);
-            }
-        } else {
-            for (let i = startValue; i >= endValue; i = i + step) {
-                array.push(i);
-            }
+        for (let i = arr.length - 1; i >= 0; i--) {
+            list = {value: arr[i], rest: list};
         }
 
-        console.log(array);
-        return array;
+        return list;
     };
 
-    console.log(range(1, 10));
-    console.log(sum(range(1, 10, 3)));
-    console.log(sum(range(10, 1, -2)));
+    console.log(arrayToList([1, 2, 3]));
 });
