0278e04473555904e52f1df753d8ddb7ecae66bf
Sanya Marynych <olexandr.marinych.stratis@gmail.com>
2019-04-23 17:43:33 +0300

    learnjs

---
+++ learnjs/app/js/core/task-eight.js
@@ -0,0 +1,4 @@
+// window.addEventListener('load', () => {
+//     let map = (fn, array) => {
+//     };
+// });
--- learnjs/app/js/core/task-seven.js
+++ learnjs/app/js/core/task-seven.js
@@ -1,27 +1,57 @@
 window.addEventListener('load', () => {
-    let sequence = (start = 0, step = 1) => {
-        start -= step;
-        return () => start += step;
-    };
+    // let sequence = (start = 0, step = 1) => {
+    //     start -= step;
+    //     return () => start += step;
+    // };
+    //
+    // let generator = sequence(10, 3);
+    //
+    // console.log(generator());
+    // console.log(generator());
+    // console.log(generator());
+    //
+    // let take = (fn, count) => {
+    //     let array = [];
+    //
+    //     while(count) {
+    //         array.push(fn());
+    //         count--;
+    //     }
+    //
+    //     return array;
+    // };
+    //
+    // let gen2 = sequence(0, 2);
+    //
+    // console.log(take(gen2, 5));
 
-    let generator = sequence(10, 3);
+    let f = (...args) => args.length ? 1 : 0;
 
-    console.log(generator());
-    console.log(generator());
-    console.log(generator());
+    console.log(f(undefined));
+    console.log(f());
 
-    let take = (fn, count) => {
-        let array = [];
 
-        while(count) {
-            array.push(fn());
-            count--;
-        }
+    // let sum = (...args) => args.reduce((prev, current) => prev + current, 0);
+    //
+    // console.log(sum());
+    // console.log(sum(1));
+    // console.log(sum(1, 2));
+    // console.log(sum(1, 2, 3));
+    // console.log(sum(1, 2, 3, 4));
 
-        return array;
-    };
+    // let myConcat = (...args) => {
+    //     let array = [];
+    //
+    //     for (let i = 0; i <= args.length; i++) {
+    //         if (i > 0) {
+    //             array.push(args[i]);
+    //         }
+    //     }
+    //
+    //     return array.join(args[0]);
+    // };
 
-    let gen2 = sequence(0, 2);
-
-    console.log(take(gen2, 5));
+    // console.log(myConcat(', ', 'red', 'orange', 'blue'));
+    // console.log(myConcat('; ', 'red', 'orange', 'blue'));
+    // console.log(myConcat('. ', 'red', 'orange', 'blue'));
 });
