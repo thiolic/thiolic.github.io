510c59176340077932d6271ab9e7780d049a7cb8
Sanya Marynych <olexandr.marinych.stratis@gmail.com>
2019-05-23 17:00:42 +0300

    learnjs

--- learnjs/app/js/core/todo.js
+++ learnjs/app/js/core/todo.js
@@ -1,12 +1,12 @@
 window.addEventListener('load', () => {
-    let btnAdd = document.querySelector('.todo__add');
+    let mainWrapper = document.querySelector('.todo');
+    let todoList = document.querySelector('.todo__list');
 
     let createElement = () => {
         let input = document.querySelector('.todo__input');
         let inputValue = input.value;
-        let todoList = document.querySelector('.todo__list');
         let newLi = document.createElement('li');
-        let liStructure = '<span class="todo__item-text"></span>' +
+        let liStructure = '<span class="todo__item-text" contenteditable="false"></span>' +
                         '<div class="todo__item-buttons">' +
                         '<button class="todo__item-button -edit">Edit</button>' +
                         '<button class="todo__item-button -remove">Remove</button>' +
@@ -20,7 +20,40 @@ window.addEventListener('load', () => {
         }
     };
 
-    btnAdd.addEventListener('click', () => {
-        createElement();
+    let removeElement = (elem) => {
+        todoList.removeChild(elem.parentNode.parentNode);
+    };
+
+    let editElement = (elem) => {
+        let elemParent = elem.parentNode.parentNode;
+        let editingField = elemParent.querySelector('.todo__item-text');
+
+        if (editingField.classList.contains('-active')) {
+            editingField.classList.remove('-active');
+            editingField.setAttribute('contenteditable', 'false');
+            elem.innerHTML = 'Edit';
+        } else {
+            editingField.classList.add('-active');
+            editingField.setAttribute('contenteditable', 'true');
+            elem.innerHTML = 'Save';
+        }
+    };
+
+    mainWrapper.addEventListener('click', (event) => {
+        let target = event.target;
+
+        if (target.tagName.toLowerCase() === 'button') {
+            if (target.classList.contains('todo__add')) {
+                createElement();
+            }
+
+            if (target.classList.contains('-remove')) {
+                removeElement(target);
+            }
+
+            if (target.classList.contains('-edit')) {
+                editElement(target);
+            }
+        }
     });
 });
