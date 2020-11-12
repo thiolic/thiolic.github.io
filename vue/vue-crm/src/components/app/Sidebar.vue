28c36f5f3034c2b0577a64b3c4c8dc3bd6b79f4b
Sanya Marynych <olexandr.marinych.stratis@gmail.com>
2020-11-12 16:51:47 +0200

    - added vue project

---
+++ vue/vue-crm/src/components/app/Sidebar.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: value}">
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      tag="li"
+      active-class="active"
+      :to="link.url"
+      :exact="link.exact"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'Sidebar',
+    props: ['value'],
+    data: () => ({
+      links: [
+        {
+          title: 'Счет',
+          url: '/',
+          exact: true
+        },
+        {
+          title: 'История',
+          url: '/history'
+        },
+        {
+          title: 'Планирование',
+          url: '/planning'
+        },
+        {
+          title: 'Новая запись',
+          url: '/record'
+        },
+        {
+          title: 'Категории',
+          url: '/categories'
+        },
+      ]
+    })
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/vue-crm/src/utils/messages.js
@@ -0,0 +1,6 @@
+export default {
+  'logout': 'You\'re logged out.',
+  'auth/user-not-found': 'There is no user record corresponding to this identifier. The user may have been deleted.',
+  'auth/wrong-password': 'The password is invalid or the user does not have a password.',
+  'auth/email-already-in-use': 'The email address is already in use by another account.'
+}
