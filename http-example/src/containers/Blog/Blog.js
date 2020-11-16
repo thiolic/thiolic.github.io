1b94e71533682352814525fbcf0feeba33f504c1
Sanya Marynych <olexandr.marinych.stratis@gmail.com>
2020-11-16 16:24:53 +0200

    React - Router

--- http-example/src/containers/Blog/Blog.js
+++ http-example/src/containers/Blog/Blog.js
@@ -3,7 +3,9 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts'
-import NewPost from "./NewPost/NewPost";
+import asyncComponent from "../../hoc/asyncComponent";
+// import NewPost from "./NewPost/NewPost";
+const AsyncNewPost = asyncComponent(() => import('./NewPost/NewPost'));
 
 class Blog extends Component {
     state = {
@@ -27,7 +29,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    {auth ? <Route path="/new-post" component={NewPost} /> : null}
+                    {auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                 </Switch>
